372d8b65cadbb7f57974c2a4c73d4f1d5468b9ad
g <fd>
2019-09-30 17:32:27 +0800

    add resolver & typeDefs

--- index.js
+++ index.js
@@ -3,17 +3,139 @@ const { ApolloServer, gql } = require('apollo-server-express');
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
-  type Query {
-    hello: String
-  }
+
+    type Query {
+        "測試用 Hello World"
+        hello: String
+        "取得目前使用者"
+        me: User
+        "取得所有使用者"
+        users: [User]
+        "依照名字取得特定使用者"
+        user(name: String!): User
+        "取得所有貼文"
+        posts: [Post]
+        "依照 id 取得特定貼文"
+        post(id: ID!): Post
+    }
+
+
+    """
+    使用者
+    """
+    type User {
+        "識別碼"
+        id: ID!
+        "帳號 email"
+        email: String!
+        "名字"
+        name: String
+        "年齡"
+        age: Int
+        "朋友"
+        friends: [User]
+        "貼文"
+        posts: [Post]
+    }
+    """
+    貼文
+    """
+    type Post {
+        "識別碼"
+        id: ID!
+        "作者"
+        author: User
+        "標題"
+        title: String
+        "內容"
+        body: String
+        "按讚者"
+        likeGivers: [User]
+        "建立時間 (ISO 格式)"
+        createdAt: String
+    }
 `;
 
+const meId = 1;
+const users = [
+  {
+    id: 1,
+    email: 'fong@test.com',
+    password: '$2b$04$wcwaquqi5ea1Ho0aKwkZ0e51/RUkg6SGxaumo8fxzILDmcrv4OBIO', // 123456
+    name: 'Fong',
+    age: 23,
+    friendIds: [2, 3]
+  },
+  {
+    id: 2,
+    email: 'kevin@test.com',
+    passwrod: '$2b$04$uy73IdY9HVZrIENuLwZ3k./0azDvlChLyY1ht/73N4YfEZntgChbe', // 123456
+    name: 'Kevin',
+    age: 40,
+    friendIds: [1]
+  },
+  {
+    id: 3,
+    email: 'mary@test.com',
+    password: '$2b$04$UmERaT7uP4hRqmlheiRHbOwGEhskNw05GHYucU73JRf8LgWaqWpTy', // 123456
+    name: 'Mary',
+    age: 18,
+    friendIds: [1]
+  }
+];
+
+const posts = [
+  {
+    id: 1,
+    authorId: 1,
+    title: 'Hello World',
+    body: 'This is my first post',
+    likeGiverIds: [1, 2],
+    createdAt: '2018-10-22T01:40:14.941Z'
+  },
+  {
+    id: 2,
+    authorId: 2,
+    title: 'Nice Day',
+    body: 'Hello My Friend!',
+    likeGiverIds: [1],
+    createdAt: '2018-10-24T01:40:14.941Z'
+  }
+];
+
 // Provide resolver functions for your schema fields
+const filterPostsByUserId = userId =>
+  posts.filter(post => userId === post.authorId);
+
+const filterUsersByUserIds = userIds =>
+  users.filter(user => userIds.includes(user.id));
+  
+const findUserByUserId = userId => users.find(user => user.id === Number(userId));
+
+const findUserByName = name => users.find(user => user.name === name);
+
+const findPostByPostId = postId => posts.find(post => post.id === Number(postId));
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    hello: () => "world",
+    me: () => findUserByUserId(meId),
+    users: () => users,
+    user: (root, { name }, context) => findUserByName(name),
+    posts: () => posts,
+    post: (root, { id }, context) => findPostByPostId(id)
   },
+  User: {
+    posts: (parent, args, context) => filterPostsByUserId(parent.id),
+    friends: (parent, args, context) => filterUsersByUserIds(parent.friendIds || [])
+  },
+  Post: {
+    author: (parent, args, context) => findUserByUserId(parent.authorId),
+    likeGivers: (parent, args, context) =>
+      filterUsersByUserIds(parent.likeGiverIds)
+  }
 };
+
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
